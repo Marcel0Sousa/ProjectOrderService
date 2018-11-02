ef4147ae278c27a594e26b1e54785f55260bded4
tchelo <tchelo@tchelo>
2018-11-02 19:33:43 -0300

    exemplo

---
+++ prjOS/src/prjOS/ddo.java
@@ -0,0 +1,5 @@
+package prjOS;
+
+public interface ddo {
+
+}
---
+++ prjOS/src/prjOS/telas.java
@@ -0,0 +1,10 @@
+package prjOS;
+
+public class telas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("oi");
+	}
+
+}
